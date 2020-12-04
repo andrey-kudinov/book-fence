63043a27438eabbc69c9d814ccf95ea8a3cd2bb1
AndreyKudinov63 <64890665+AndreyKudinov63@users.noreply.github.com>
2020-12-04 22:10:40 +0400

    первый коммит

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Select
+
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
